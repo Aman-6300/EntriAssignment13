814782f3591563876842ec7a7b4b603517b2e097
Aman-6300 <amansayooj03@gmail.com>
2024-11-07 08:28:06 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['bootstrap'] // This tells Vite/Rollup to treat 'bootstrap' as an external dependency
+    }
+  }
 })
