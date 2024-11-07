d6732c81cdfac91974304ba5d33680755fc0e804
Aman-6300 <amansayooj03@gmail.com>
2024-11-07 08:19:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Footer from './components/Footer';
+import './App.css';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+      <Projects/>
+      <Contact/>
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
